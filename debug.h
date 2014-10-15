58a0639d0156e9f0af5617413536aa2548fc7ca6
Edward <eddiecorrigall@gmail.com>
2014-10-15 14:15:30 -0400

    Improved appearance of tabulation.

--- debug.h
+++ debug.h
@@ -3,11 +3,11 @@
 
 #include "ptypes.h"
 
-#define DEBUG_BUFFER_INITIAL_BYTE	(0xFF)
-#define DEBUG_BUFFER_LENGTH			(100)
+#define DEBUG_BUFFER_INITIAL_BYTE (0xFF)
+#define DEBUG_BUFFER_LENGTH       (100)
 
-extern void	DEBUG_ClearBuffer(UINT8 initial_byte);
-extern void	DEBUG_SaveToBuffer(UINT8 byte);
-extern void	DEBUG_ReturnBuffer(UINT8 *ordered_buffer);
+extern void     DEBUG_ClearBuffer(UINT8 initial_byte);
+extern void     DEBUG_SaveToBuffer(UINT8 byte);
+extern void     DEBUG_ReturnBuffer(UINT8 *ordered_buffer);
 
 #endif
